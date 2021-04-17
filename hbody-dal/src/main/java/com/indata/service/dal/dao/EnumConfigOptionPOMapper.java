52a763502e18178bf4079f14f97e6972c97a4415
YangQi <yq990312@qq.com>
2021-04-17 19:10:30 +0800

    feat: 添加灰度配置表 #2021-04-17

---
+++ hbody-dal/src/main/java/com/indata/service/dal/dao/EnumConfigOptionPOMapper.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  Copyright 2020 byai.com All right reserved. This software is the
+ *  confidential and proprietary information of byai.com ("Confidential
+ *  Information"). You shall not disclose such Confidential Information and shall
+ *  use it only in accordance with the terms of the license agreement you entered
+ *  into with byai.com.
+ * /
+ */
+
+package com.indata.service.dal.dao;
+
+
+import com.indata.service.dal.entity.EnumConfigOptionPO;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+
+/**
+ * @author yangqi
+ */
+public interface EnumConfigOptionPOMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(EnumConfigOptionPO record);
+
+    int insertSelective(EnumConfigOptionPO record);
+
+    EnumConfigOptionPO selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(EnumConfigOptionPO record);
+
+    int updateByPrimaryKey(EnumConfigOptionPO record);
+
+    /**
+     * ===========================手动添加方法开始分界线==========================
+     */
+
+    List<EnumConfigOptionPO> getListByType(@Param("configType") String type);
+
+    EnumConfigOptionPO getByTypeAndKey(@Param("configType") String configType, @Param("configKey") String configKey);
+
+    List<EnumConfigOptionPO> getListByKey(@Param("configKey")String configKey);
+
+
+    EnumConfigOptionPO getConfigByCompany(@Param("configType")String configType,@Param("configKey")String configKey);
+}
---
+++ hbody-dal/src/main/java/com/indata/service/dal/entity/EnumConfigOptionPO.java
@@ -0,0 +1,66 @@
+/*
+ *
+ *  Copyright 2020 byai.com All right reserved. This software is the
+ *  confidential and proprietary information of byai.com ("Confidential
+ *  Information"). You shall not disclose such Confidential Information and shall
+ *  use it only in accordance with the terms of the license agreement you entered
+ *  into with byai.com.
+ * /
+ */
+
+package com.indata.service.dal.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author 杨齐
+ */
+@Data
+public class EnumConfigOptionPO implements Serializable {
+    /**
+     * INTEGER(10) 必填
+     * 主键
+     */
+    private Integer id;
+
+    /**
+     * VARCHAR(64) 必填
+     *
+     */
+    private String configType;
+
+    /**
+     * VARCHAR(64) 默认值[] 必填
+     * 标识符
+     */
+    private String configKey;
+
+    /**
+     * VARCHAR(1024) 默认值[] 必填
+     * 值
+     */
+    private String configValue;
+
+    /**
+     * VARCHAR(1024) 默认值[]
+     * 描述
+     */
+    private String description;
+
+    /**
+     * TIMESTAMP(19) 必填
+     * 创建时间
+     */
+    private Date gmtCreate;
+
+    /**
+     * TIMESTAMP(19) 必填
+     * 更新时间
+     */
+    private Date gmtModified;
+
+    private static final long serialVersionUID = 1L;
+}
